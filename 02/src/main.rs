51d5714fd1086d32ca18a0ebf0908b59b43cecb5
Ac5000 <15003210+Ac5000@users.noreply.github.com>
2025-12-03 01:39:27 -0500

    Day 2 Part 1 complete.

--- 02/src/main.rs
+++ 02/src/main.rs
@@ -25,10 +25,45 @@ impl ProductID {
     fn sum_invalids(&self) -> u64 {
         let mut sum: u64 = 0;
         for id in &self.invalid_ids {
-            sum = sum + id
+            sum += id
         }
         sum
     }
+
+    /// Adds the invalid id to the struct.
+    fn push_invalid(&mut self, number: u64) {
+        self.invalid_ids.push(number);
+    }
+
+    /// Run through all the ProductIDs and add the invalids to the vec.
+    fn find_invalids(&mut self) {
+        for sequence in self.start..self.end + 1 {
+            if check_invalid(sequence) {
+                self.push_invalid(sequence);
+            }
+        }
+    }
+}
+
+/// Check if number has even number of digits
+fn even_digits(number: u64) -> bool {
+    let num_digits = number.checked_ilog10().unwrap_or(0) + 1;
+    if num_digits.rem_euclid(2) == 0 {
+        true
+    } else {
+        false
+    }
+}
+
+/// Checks if number is invalid by turning it into a vec of chars and splitting
+/// the vec in two. If the vecs match, it is invalid. Returns true if invalid.
+fn check_invalid(number: u64) -> bool {
+    if !even_digits(number) {
+        return false;
+    };
+    let mut characters1: Vec<char> = number.to_string().chars().collect();
+    let characters2 = characters1.split_off(characters1.len() / 2);
+    characters1 == characters2
 }
 
 /// Parse the incoming file to Vec of ProductID's
@@ -49,10 +84,11 @@ fn parse_text(string: &String) -> Vec<ProductID> {
 /// twice.
 fn part1(file_name: &str) -> u64 {
     let file_contents = std::fs::read_to_string(file_name).expect("Couldn't open file");
-    let product_ids = parse_text(&file_contents);
+    let mut product_ids = parse_text(&file_contents);
     let mut sum = 0;
-    for id in product_ids {
-        sum = sum + id.sum_invalids();
+    for id in product_ids.iter_mut() {
+        id.find_invalids();
+        sum += id.sum_invalids();
     }
     sum
 }
@@ -73,13 +109,11 @@ mod tests {
         assert_eq!(part1("example1.txt"), 1227775554);
     }
 
-    /// Test for part 1.
     #[test]
     fn test_part1() {
-        assert_eq!(part1("input.txt"), 1227775554);
+        assert_eq!(part1("input.txt"), 13108371860);
     }
 
-    /// Test parse_text.
     #[test]
     fn test_parse_text() {
         let input = "11-22,95-115".to_string();
@@ -87,11 +121,32 @@ mod tests {
         assert_eq!(parse_text(&input), expected);
     }
 
-    /// Test sum_invalids.
     #[test]
     fn test_sum_invalids() {
         let mut id = ProductID::new(11, 22);
         id.invalid_ids = vec![11, 22];
         assert_eq!(id.sum_invalids(), 33);
+    }
+
+    #[test]
+    fn test_even_digits() {
+        assert_eq!(even_digits(5), false);
+        assert_eq!(even_digits(10), true);
+        assert_eq!(even_digits(101), false);
+    }
+
+    #[test]
+    fn test_check_invalid() {
+        assert_eq!(check_invalid(1), false);
+        assert_eq!(check_invalid(10), false);
+        assert_eq!(check_invalid(11), true);
+        assert_eq!(check_invalid(111), false);
+    }
+
+    #[test]
+    fn test_find_invalids() {
+        let mut id = ProductID::new(11, 22);
+        id.find_invalids();
+        assert_eq!(id.invalid_ids, vec![11, 22]);
     }
 }
