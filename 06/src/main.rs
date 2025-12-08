2d79e5f152aecaf76623d62fd6c1beaf0ab5de79
Ac5000 <15003210+Ac5000@users.noreply.github.com>
2025-12-08 18:25:39 -0500

    Day 06 Part 2 Complete.

--- 06/src/main.rs
+++ 06/src/main.rs
@@ -1,6 +1,7 @@
 //! Day 06: Trash Compactor
 
 use std::collections::HashMap;
+use utilities::{coord::Coord, grid::Grid};
 
 /// Enum that represents the operation to take place on a column.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
@@ -34,11 +35,14 @@ struct Column {
 }
 
 impl Column {
-    /// Make a new Column from Vec(row, &str).
-    fn new_from_raw(mut entry: Vec<&str>) -> Self {
+    /// Make a new Column from Vec(&str).
+    fn new_from_raw(entry: Vec<&str>) -> Self {
         let mut ret = Self::default();
-        ret.operation = Operation::from(entry.pop().expect("Failed to pop Operation."));
         for number in entry {
+            if number == "+" || number == "*" {
+                ret.operation = Operation::from(number);
+                continue;
+            };
             ret.numbers.push(
                 number
                     .parse()
@@ -78,6 +82,33 @@ fn parse_text(string: &String) -> HashMap<usize, Column> {
     columns
 }
 
+/// Parse the incoming file to HashMap of column#,Column.
+fn parse_text2(string: &String) -> HashMap<usize, Column> {
+    let mut rows_raw: HashMap<usize, Vec<&str>> = HashMap::new();
+    let mut rows: HashMap<usize, Column> = HashMap::new();
+    let mut column_count: usize = 0;
+
+    for line in string.lines() {
+        // Increment the "column" count each empty line.
+        if line.split_whitespace().count() == 0 {
+            column_count += 1;
+            continue;
+        }
+
+        for item in line.split_whitespace() {
+            rows_raw
+                .entry(column_count)
+                .and_modify(|x| x.push(item))
+                .or_insert_with(|| vec![item]);
+        }
+    }
+
+    for (row, items) in rows_raw {
+        rows.insert(row, Column::new_from_raw(items));
+    }
+    rows
+}
+
 /// For each column of numbers, perform the operation at the bottom. Sum all results.
 fn part1(file_name: &str) -> u64 {
     let file_contents = std::fs::read_to_string(file_name).expect("Couldn't open file");
@@ -86,12 +117,68 @@ fn part1(file_name: &str) -> u64 {
     answer
 }
 
+/// Rotate a grid counter-clockwise.
+/// So this doesn't really rotate, it sorta flips the 2d matrix, but it works for
+/// what I need...
+fn rotate_grid_ccw(grid: &Grid) -> Grid {
+    let mut new_grid = Grid::new();
+    for (&coord, &char_val) in grid.char_map.iter() {
+        let new_x = coord.y;
+        let new_y = coord.x;
+
+        let new_coord = Coord::new(new_x, new_y);
+        new_grid.char_map.insert(new_coord, char_val);
+    }
+    new_grid.max_x = grid.max_y;
+    new_grid.max_y = grid.max_x;
+
+    new_grid
+}
+
+/// Convert the grid back to a string so we can use it in the logic that worked
+/// for part1.
+fn grid_to_string(grid: &Grid) -> String {
+    let mut ret = String::new();
+    for y in 0..=grid.max_y {
+        for x in 0..=grid.max_x {
+            let character = *grid
+                .char_map
+                .get(&Coord::new(x, y))
+                .expect(&format!("Failed to find char at: {:?}", (x, y)));
+            // Need to add a space before the operators so the parse by split_whitespace
+            // works.
+            if character == '+' || character == '*' {
+                ret.push(' ');
+                ret.push(character);
+                ret.push(' ');
+            } else {
+                ret.push(character);
+            };
+        }
+        // Push newline for each y/row.
+        ret.push_str("\n");
+    }
+
+    ret
+}
+
+/// For each column of numbers, perform the operation at the bottom. Sum all results.
+/// Read numbers right-to-left in each column.
+fn part2(file_name: &str) -> u64 {
+    let grid = Grid::new_from_file(file_name);
+    let new_grid = rotate_grid_ccw(&grid);
+    let new_string = grid_to_string(&new_grid);
+    let columns = parse_text2(&new_string);
+    let answer = columns.into_iter().map(|x| get_result(x.1)).sum();
+    answer
+}
+
 /// Main function / code entry point.
 fn main() {
     println!("Sum for example1: {}", part1("example1.txt"));
     println!("Sum for input: {}", part1("input.txt"));
-    // println!("Sum for example1 part2: {}", part2("example1.txt"));
-    // println!("Sum for input part2: {}", part2("input.txt"));
+    println!("Sum for example1 part2: {}", part2("example1.txt"));
+    println!("Sum for input part2: {}", part2("input.txt"));
 }
 
 #[cfg(test)]
@@ -109,14 +196,14 @@ mod tests {
         assert_eq!(part1("input.txt"), 4583860641327);
     }
 
-    // /// Test against the example.
-    // #[test]
-    // fn part2_example01() {
-    //     assert_eq!(part2("example1.txt"), 14);
-    // }
-    //
-    // #[test]
-    // fn test_part2() {
-    //     assert_eq!(part2("input.txt"), 338258295736104);
-    // }
+    /// Test against the example.
+    #[test]
+    fn part2_example01() {
+        assert_eq!(part2("example1.txt"), 3263827);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("input.txt"), 11602774058280);
+    }
 }
