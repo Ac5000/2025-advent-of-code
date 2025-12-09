638a9d8c4365b6bbd0240514e72d9ae8d579cb1f
Ac5000 <15003210+Ac5000@users.noreply.github.com>
2025-12-08 19:38:31 -0500

    Day 7 Part 1 Complete.

---
+++ 07/src/main.rs
@@ -0,0 +1,106 @@
+//! Day 07: Laboratories
+
+use utilities::{coord::Coord, grid::Grid};
+
+/// Check if the beam is above this coordinate. E.g., do we care about this coord?
+fn is_beam_above(coord: &Coord, grid: &Grid) -> bool {
+    if grid
+        .char_map
+        .get(&coord.north())
+        .is_some_and(|&x| x == '|' || x == 'S')
+    {
+        true
+    } else {
+        false
+    }
+}
+
+/// Check if this Coord will split the beam.
+fn check_split(coord: &Coord, grid: &Grid) -> bool {
+    if grid.char_map.get(coord).is_some_and(|&x| x == '^') {
+        if is_beam_above(coord, grid) {
+            return true;
+        }
+    }
+    false
+}
+
+/// Splits the beam at the given coordinate by placing '|' on left/right.
+fn split_beam(coord: &Coord, grid: &mut Grid) {
+    let left = coord.west();
+    let right = coord.east();
+    grid.char_map.entry(left).and_modify(|x| *x = '|');
+    grid.char_map.entry(right).and_modify(|x| *x = '|');
+}
+
+/// Check if the beam should continue.
+fn check_continues(coord: &Coord, grid: &Grid) -> bool {
+    if grid.char_map.get(coord).is_some_and(|&x| x == '.') {
+        if is_beam_above(coord, &grid) {
+            return true;
+        }
+    }
+    false
+}
+
+/// Continue the beam through this coordinate.
+fn continue_beam(coord: Coord, grid: &mut Grid) {
+    grid.char_map.entry(coord).and_modify(|x| *x = '|');
+}
+
+/// Figure out how many times the beam splits.
+fn part1(file_name: &str) -> u64 {
+    let mut grid = Grid::new_from_file(file_name);
+    let mut splits = 0;
+    for y in 0..=grid.max_y {
+        for x in 0..=grid.max_x {
+            let coord = Coord::new(x, y);
+            if !is_beam_above(&coord, &grid) {
+                continue;
+            }
+            if check_continues(&coord, &grid) {
+                continue_beam(coord, &mut grid);
+            }
+            if check_split(&coord, &grid) {
+                split_beam(&coord, &mut grid);
+                splits += 1;
+            }
+        }
+    }
+    splits
+}
+
+/// Main function / code entry point.
+fn main() {
+    println!("Sum for example1: {}", part1("example1.txt"));
+    println!("Sum for input: {}", part1("input.txt"));
+    // println!("Sum for example1 part2: {}", part2("example1.txt"));
+    // println!("Sum for input part2: {}", part2("input.txt"));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// Test against the example.
+    #[test]
+    fn part1_example01() {
+        assert_eq!(part1("example1.txt"), 21);
+    }
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1("input.txt"), 1649);
+    }
+
+    // /// Test against the example.
+    // #[test]
+    // fn part2_example01() {
+    //     assert_eq!(part2("example1.txt"), 3263827);
+    // }
+    //
+    // #[test]
+    // fn test_part2() {
+    //     assert_eq!(part2("input.txt"), 11602774058280);
+    // }
+}
